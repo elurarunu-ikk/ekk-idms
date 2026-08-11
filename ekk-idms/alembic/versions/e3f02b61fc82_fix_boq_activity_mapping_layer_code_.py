9dd12ea5a9743fb0e240ecc682b0a1719d7b9d88
Arunprakash <elurarunu@gmail.com>
2026-08-11 11:22:59 +0530

    fix: make boq_activity_mapping.layer_code nullable

---
+++ ekk-idms/alembic/versions/e3f02b61fc82_fix_boq_activity_mapping_layer_code_.py
@@ -0,0 +1,28 @@
+"""fix_boq_activity_mapping_layer_code_nullable
+
+Revision ID: e3f02b61fc82
+Revises: c51d257406b2
+Create Date: 2026-08-11 05:51:51.230058
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+revision: str = 'e3f02b61fc82'
+down_revision: Union[str, Sequence[str], None] = 'c51d257406b2'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.alter_column('boq_activity_mapping', 'layer_code',
+                    existing_type=sa.String(length=50),
+                    nullable=True)
+
+
+def downgrade() -> None:
+    op.alter_column('boq_activity_mapping', 'layer_code',
+                    existing_type=sa.String(length=50),
+                    nullable=False)
